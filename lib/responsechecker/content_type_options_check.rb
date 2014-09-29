08bbfe0d23d1be3791c39ccd057ae0bdbb82f54e
Najaf Ali <ali.najaf@gmail.com>
2014-09-29 10:33:10 +0100

    Made tests run correctly

--- lib/responsechecker/content_type_options_check.rb
+++ lib/responsechecker/content_type_options_check.rb
@@ -1,12 +1,14 @@
-class ContentTypeOptionsCheck
-  def name
-    'X-Content-Type-Options'
-  end
+module ResponseChecker
+  class ContentTypeOptionsCheck
+    def name
+      'X-Content-Type-Options'
+    end
 
-  def description
-  end
+    def description
+    end
 
-  def perform(_, response)
-    'nosniff' == response['x-content-type-options']
+    def perform(_, response)
+      'nosniff' == response['x-content-type-options']
+    end
   end
 end
